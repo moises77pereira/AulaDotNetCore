2ea2127b535a25d1825dea003a701b5f9c2cdd69
moises77pereira <moisescerqueira77@gmail.com>
2019-09-17 11:36:24 -0300

    Inclusão das classes login

--- AulaDotNetCore.Web/ClientApp/src/app/produto/produto.component.ts
+++ AulaDotNetCore.Web/ClientApp/src/app/produto/produto.component.ts
@@ -1,1 +1,14 @@
-﻿
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "app-produto",
+  template:"<html><body>{{ obterNome()}}</body></html>"
+})
+export class ProdutoComponent {
+   public nome: string;
+   public liberadoParaVenda: boolean;
+
+   public obterNome():string {
+     return "Samsung";
+   }
+}
---
+++ AulaDotNetCore.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "app-login",
+  template: "./login.component.html",
+  styleUrls:["./login.component.css"]
+})
+export class LoginComponent {
+
+}
